2809c17a8b5253525d27d2d8fe6de372bbf200c6
Andy Bool <andybool50@gmail.com>
2024-06-18 16:49:00 +0100

    formatted basic layout of page with json info included

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ fetch('portfolio.json').then(function (res) {
         const projectType = document.querySelector('.project')
         projectType.innerHTML += `
     <div class="specificProject">
-            <h2>${project.name}</h2>
+            <h2 class="p-3 text-2xl">${project.name}</h2>
         <div>
             <img src="${project.img}" alt="A ${project.name}" class="projectImage" />
         </div>
         <div class="projectText">
-            <p>Description</p>
-            <p>${project.desc}
-            <p><a class="github" href="${project.url}" target="_blank">See it on github</a></p>
+            <p class="p-3 text-2xl">Description</p>
+            <p class="p-3 text-xl">${project.desc}
+            <p p-3><a class="p-3 text-xl" href="${project.url}" target="_blank">See it on github</a></p>
         </div>
     </div>`
     })
