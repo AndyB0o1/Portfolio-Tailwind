8237e3070381e0cf2c208d45ed5d17770b9db883
Andy Bool <andybool50@gmail.com>
2024-06-19 16:55:12 +0100

    restyled page to a differenct colour scheme and added interactivity to project images

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ fetch('portfolio.json').then(function (res) {
         console.log(project)
         const projectType = document.querySelector('.project')
         projectType.innerHTML += `
-    <div class="specificProject max-w-xl bg-gradient-to-b from-lime-100 to-lime-300 opacity-80 border solid border white rounded-lg p-3">
-            <h2 class="p-3 text-amber-700 text-2xl">${project.name}</h2>
+    <div class="specificProject bg-[#90AEAD] max-w-xl border solid border white rounded-lg p-3">
+            <h2 class="p-3 font-bold text-cyan-900 text-3xl">${project.name}</h2>
         <div>
-            <img src="${project.img}" alt="A ${project.name}" class="projectImage" />
+            <img src="${project.img}" alt="A ${project.name}" class="projectImage blur-sm hover:blur-none" />
         </div>
         <div class="projectText">
-            <p class="p-3 text-amber-700 text-2xl">Description</p>
-            <p class="p-3 text-amber-700 text-xl">${project.desc}
-            <p p-3><a class="p-3 text-amber-700 text-xl" href="${project.url}" target="_blank">See it on github</a></p>
+            <p class="p-3 font-bold text-cyan-900 text-2xl">Description</p>
+            <p class="p-3 text-xl">${project.desc}
+            <p p-3><a class="p-3 text-orange-100 text-xl hover:bg-lime-300 hover:text-cyan-900 rounded-lg p-3" href="${project.url}" target="_blank">See it on github</a></p>
         </div>
     </div>`
     })
