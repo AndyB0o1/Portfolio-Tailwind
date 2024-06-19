ba4994e43ff5fd3871ba3a309414dc3afa07f0c0
Andy Bool <andybool50@gmail.com>
2024-06-19 09:29:22 +0100

    added background image to portfolio seciton and new image in about me section

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ fetch('portfolio.json').then(function (res) {
         console.log(project)
         const projectType = document.querySelector('.project')
         projectType.innerHTML += `
-    <div class="specificProject">
-            <h2 class="p-3 text-2xl">${project.name}</h2>
+    <div class="specificProject bg-orange-100 opacity-80 border solid border white rounded-lg p-3">
+            <h2 class="p-3 text-amber-700 text-2xl">${project.name}</h2>
         <div>
             <img src="${project.img}" alt="A ${project.name}" class="projectImage" />
         </div>
         <div class="projectText">
-            <p class="p-3 text-2xl">Description</p>
-            <p class="p-3 text-xl">${project.desc}
-            <p p-3><a class="p-3 text-xl" href="${project.url}" target="_blank">See it on github</a></p>
+            <p class="p-3 text-amber-700 text-2xl">Description</p>
+            <p class="p-3 text-amber-700 text-xl">${project.desc}
+            <p p-3><a class="p-3 text-amber-700 text-xl" href="${project.url}" target="_blank">See it on github</a></p>
         </div>
     </div>`
     })
