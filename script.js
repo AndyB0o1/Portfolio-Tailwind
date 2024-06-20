8b6b1e53428e8b10eac36a158185ee6e971e79ac
Andy Bool <andybool50@gmail.com>
2024-06-20 10:36:00 +0100

    modified responsive design on the header and portfolio section for mobile size screen

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ fetch('portfolio.json').then(function (res) {
         console.log(project)
         const projectType = document.querySelector('.project')
         projectType.innerHTML += `
-    <div class="specificProject basis-1/3 bg-[#90AEAD] max-w-2xl border solid border white rounded-lg p-3">
+    <div class="specificProject sm:basis-1/3 bg-[#90AEAD] max-w-2xl border solid border white rounded-lg p-3">
             <h2 class="p-3 font-bold text-cyan-900 text-3xl">${project.name}</h2>
         <div>
-            <img src="${project.img}" alt="A ${project.name}" class="projectImage blur-sm hover:blur-none" />
+            <img src="${project.img}" alt="A ${project.name}" class="projectImage" />
         </div>
         <div class="projectText">
             <p class="p-3 font-bold text-cyan-900 text-2xl">Description</p>
             <p class="p-3 text-xl">${project.desc}
-            <p class="py-2"><a class="p-3 text-orange-100 text-xl bg-cyan-900 rounded-lg hover:bg-lime-300 hover:text-cyan-900 rounded-lg p-3" href="${project.url}" target="_blank">See it on github</a></p>
+            <p class="py-2"><a class="p-3 text-orange-100 text-l bg-cyan-900 rounded-lg sm:text-xl hover:bg-lime-300 hover:text-cyan-900 rounded-lg p-3" href="${project.url}" target="_blank">See it on github</a></p>
         </div>
     </div>`
     })
